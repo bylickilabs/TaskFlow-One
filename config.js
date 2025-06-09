b32ef8b43fd57be38fd7f3ee2080775403a47a39
AlphaDevelopment✔️ <109308073+bylickilabs@users.noreply.github.com>
2025-06-09 22:05:52 +0200

    Add taskFlow One
    Co-Authored-By: AlphaDevelopment✔️ <109308073+bylickilabs@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,61 @@
+const CONFIG = {
+    passwordProtectionEnabled: 	true,
+    password: 					"test123",
+    
+
+    showGitHubRepos:			 true,
+    githubRepos: [
+        {
+            username: 		"bylickilabs",
+            repo: 			"LINK",
+            displayName: 	"REPO NAME"
+        },
+        {
+            username: 		"bylickilabs",
+            repo: 			"LINK",
+            displayName: 	"REPO NAME"
+        },
+        {
+            username: 		"bylickilabs",
+            repo: 			"LINK",
+            displayName: 	"REPO NAME"
+        }
+    ],
+
+
+    showSocialIcons: true,
+    socialMediaLinks: {
+        behance: 		"",
+        bitbucket: 		"",
+        discord: 		"https://discord.gg/MfTE2TSuWP",
+        dribbble: 		"",
+        email: 			"",
+        facebook: 		"",
+        github: 		"https://github.com/bylickilabs",
+        gitlab: 		"",
+        instagram: 		"",
+        linkedin: 		"",
+        medium: 		"",
+        phone: 			"",
+        pinterest: 		"",
+        quora: 			"",
+        reddit: 		"",
+        rss: 			"",
+        skype: 			"",
+        slack: 			"",
+        snapchat: 		"",
+        soundcloud: 	"",
+        spotify: 		"",
+        stackexchange: 	"",
+        stackoverflow: 	"",
+        telegram: 		"",
+        tiktok: 		"",
+        twitch: 		"",
+        twitter: 		"",
+        vimeo: 			"",
+        website: 		"https://www.bylickilabs.de",
+        whatsapp: 		"",
+        wordpress: 		"",
+        youtube: 		"https://www.youtube.com/@AlphaDevelopmentTeam"
+    }	
+};
